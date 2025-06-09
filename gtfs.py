a214fde7d657f73129324a39aecf879e08322678
Gavin Yancey <gyancey@hmc.edu>
2025-06-09 12:20:42 -0700

    parse headers, better CSV handling

--- gtfs.py
+++ gtfs.py
@@ -13,7 +13,7 @@ def main():
 
     p.add_argument('gtfs', type=os.path.realpath, nargs='+', help='Path to gtfs directory')
     p.add_argument('output', help='Image file to save')
-    p.add_argument('--maxdim', type=int, default=1000)
+    p.add_argument('--maxdim', '--width', type=int, default=1000)
     p.add_argument('--color', action='store_true')
     p.add_argument('--open', action='store_true')
 
@@ -40,18 +40,47 @@ def main():
 def get_colors(gtfs):
     with open_file(gtfs, 'routes.txt') as routes:
         route_colors = {}
+        headers = parse_headers(routes.readline())
+        if 'route_id' not in headers or 'route_color' not in headers:
+            return {}
         for line in routes:
-            items = [stripquote(item) for item in line.rstrip('\n').split(',')]
-            if len(items) > 7 and items[7]:
-                if len(items[7]) == 6:
-                    route_colors[items[0]] = items[7]
+            items = splitline(line)
+            color_str = items[-(len(headers) - headers['route_color'])]
+            if len(color_str) == 6 and all(c in '0123456789abcdefABCDEF' for c in color_str):
+                route_colors[items[headers['route_id']]] = color_str
+            else:
+                if color_str:
+                    print(f'WARNING: unknown color {color_str}')
+                # TODO: maybe configurable fallback?
+                route_colors[items[headers['route_id']]] = '000000'
     shape_colors = {}
     with open_file(gtfs, 'trips.txt') as trips:
+        headers = parse_headers(trips.readline())
+        if 'route_id' not in headers or 'shape_id' not in headers:
+            return {}
         for line in trips:
-            items = [stripquote(item) for item in line.rstrip('\n').split(',')]
-            if len(items) > 7 and items[7] and items[0] in route_colors:
-                shape_colors[items[7]] = '#' + route_colors[items[0]]
+            items = splitline(line)
+            if items[headers['route_id']] in route_colors:
+                shape_colors[items[headers['shape_id']]] = '#' + route_colors[items[headers['route_id']]]
     return shape_colors
+
+def splitline(line):
+    return [stripquote(item.rstrip(' ')) for item in line.rstrip('\n').split(',')]
+
+def parse_headers(line):
+    if line[0] in ('\ufeff', '\ufffe'):
+        line = line[1:]
+    return {item: idx for idx,item in enumerate(splitline(line))}
+
+def parse_shapes_offsets(headers_line):
+    headers = parse_headers(headers_line)
+    return headers['shape_id'], headers['shape_pt_lat'], headers['shape_pt_lon']
+
+def parse_shape_line(line, offsets):
+    parts = splitline(line)
+    # ID, lon, -lat
+    # TODO: this is an odd ordering to make x,y work...
+    return parts[offsets[0]], float(stripquote(parts[offsets[2]])), -float(stripquote(parts[offsets[1]]))
 
 def stripquote(item):
     if item and item[0] == '"' and item[-1] == '"':
@@ -61,20 +90,20 @@ def stripquote(item):
 
 def get_bounding_box(gtfs, bounding_box=None):
     with open_file(gtfs, 'shapes.txt') as shapefile:
-        shapefile.readline()
+        offsets = parse_shapes_offsets(shapefile.readline())
         if bounding_box is not None:
             minlat = bounding_box[0]
             maxlat = bounding_box[1]
             minlon = bounding_box[2]
             maxlon = bounding_box[3]
         else:
-            _, (lat, lon) = parse_shape_line(shapefile.readline())
+            _, lat, lon = parse_shape_line(shapefile.readline(), offsets)
             minlat = lat
             maxlat = lat
             minlon = lon
             maxlon = lon
         for line in shapefile:
-            _, (lat, lon) = parse_shape_line(line)
+            _, lat, lon = parse_shape_line(line, offsets)
             if lat < minlat:
                 minlat = lat
             if lat > maxlat:
@@ -112,9 +141,9 @@ def draw_gtfs(gtfs, bounding_box, image, colors):
         lastid = None
         px = None
         py = None
-        shapefile.readline() # CSV header
+        offsets = parse_shapes_offsets(shapefile.readline())
         for line in shapefile:
-            id, (lat, lon) = parse_shape_line(line)
+            id, lat, lon = parse_shape_line(line, offsets)
             x = int((lat - minlat) * (image.width-10) / (maxlat - minlat))+5
             y = int((lon - minlon) * (image.height-10) / (maxlon - minlon))+5
             if id == lastid:
@@ -122,11 +151,6 @@ def draw_gtfs(gtfs, bounding_box, image, colors):
             px = x
             py = y
             lastid = id
-
-def parse_shape_line(line):
-    parts = line.rstrip('\n').split(',')
-    # ID, (lat, lon), seqnum
-    return parts[0], (float(stripquote(parts[2])), -float(stripquote(parts[1])))
 
 def open_file(gtfs, name):
     if os.path.isdir(gtfs):
